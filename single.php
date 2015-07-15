124f2499c2da28f1896865ed3e07e20460a4b149
Dahngeek <me@dahngeek.com>
2015-07-14 19:20:07 -0600

    added meta tag & improved single

--- single.php
+++ single.php
@@ -8,26 +8,123 @@ get_header();
     <!-- termina slider -->
     <div id="contenidoNoticiaPrincipal" class="row">
       <div id="noticiaContenido" class="large-9 medium-9 left">
-        <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
           <div class="tituloDnoticia">
-              <h1 class="tituloNoticia"><?php the_title(); ?></h1>
-              <?php
-                if ( has_post_thumbnail() ) {
-                  $large_image_url = wp_get_attachment_image_src( get_post_thumbnail_id(), 'large' );
-                  ?>
-                  <img src="<?php echo $large_image_url; ?>" alt="imagendestacada" class="imagenDestacada">
-                  <?php
-                }
-                ?>
+              <h1 class="tituloNoticia">La Música: Función, Virtudes e Influencia</h1>
+              <img src="media/musica1.jpg" alt="imagendestacada" class="imagenDestacada">
           </div>
-          <small id="postInfo"><?php setlocale(LC_TIME, "es_ES"); ?><?php echo strftime("%d de %B de %Y"); ?></small>
           <div class="contenidoNoticia">
-              <?php the_content(); ?>
-              <?php endwhile; else : ?>
-              <p><?php _e( 'No se encontró nada.' ); ?></p>
+<blockquote>
+<p>Compilación de citas de Elena de White realizada por Arturo L. White</p>
+<footer>
+<cite>
+Junio de 1992
+</cite>
+</footer>
+</blockquote>
+
+<ol>
+<li>Introducción</li>
+<li>El papel de la música</li>
+<li>El uso eficaz de la música en la experiencia de Israel</li>
+<li>Virtudes deseables</li>
+<li>Cualidades indeseables</li>
+<li>
+  La música religiosa transformada en artimaña de Satanás
+  <ol>
+    <li>La música en el congreso campestre
+de Indiana en 1900, descrita por testigos oculares</li>
+    <li>Comentarios de Elena G. de White
+acerca de la música durante el Congreso Campestre
+de Indiana de 1900</li>
+  </ol>
+</li>
+<li>La tentación de la música mundana</li>
+<li>La música secular</li>
+<li>Los que ejecutan música</li>
+<li>Testimonio a un director de coro susceptible</li>
+<li>Clave de Abreviaturas</li>
+</ol>
+
+<div id="introduccion">
+<h3>Introducción</h3>
+<p>En vista de la próxima reunión de especialistas para el estudio de la música, se pidió al Patrimonio White hacer una compilación de los artículos escritos por Elena G. de White que tienen que ver con el asunto a ser considerado por la comisión. Al hacerlo se tomó en cuenta tanto el material publicado como el inédito. Tienen que ver, mayormente según lo entendemos, con los consejos que enuncian ciertos principios, para luego aplicar estos principios a fin de que los consejos de la pluma de Elena G. de White tengan una influencia directa en el asunto que ahora nos ocupa.</p>
+<p>No intentamos hacer una presentación general en este documento, ni mucho menos una presentación exhaustiva. Es del todo probable que hayamos pasado por alto cierta declaración escogida que fue captada por algún miembro de la comisión.
+</p>
+<p>Creemos, sin embargo, que las declaraciones escogidas presentan los principios básicos, los cuales podrán ser efectivamente aplicados. Estos principios abarcan la función y las cualidades de la música, el impacto de la misma ya sea para bien o para mal, el uso efectivo de la música en los cultos de la iglesia y algunos consejos que pueden ser de interés para los compositores y directores de música.
+</p>
+<p>En la experiencia de Indiana en 1900 y 1901 a la cual se alude generalmente como el movimiento de la carne santificada (presentado en Mensajes Selectos, tomo 2, p. 35-45), la música aparece notablemente. Tenemos descripciones de testigos oculares de lo que sucedió, y también consejos de Elena de White basados en una visión que recibió en enero de 1900 en Australia, en el mismo principio del movimiento de la carne santificada, y en informes que le fueron enviados de parte del pastor Haskell y Sra., siete meses después de la visión.
+</p>
+<p>De esa combinación de materiales, encontramos ciertos principios que nos guiarán hoy.
+</p>
+<p>Así, sin querer hacer esta compilación demasiado detallada, pero tratando de incluir citas que suplen las necesidades del estudio de esta junta, presentamos este material.
+</p>
+<blockquote>
+A.L. White, Secretario <br>
+E.G. White Estate
+
+<cite>Washington, D.C. , 30 de junio de 1992.</cite>
+
+</blockquote>
+</div>
+
+<div id="papel">
+<h3>El papel de la música</h3>
+<h4>El poder del canto.</h4>
+<p>La historia de los cantos de la Biblia está llena de sugerencias en cuanto a los usos y beneficios de la música y el canto. A menudo se pervierte la música haciéndola servir a malos propósitos y de ese modo llega a ser uno de los agentes más seductores de la tentación. Pero, debidamente empleada, es un precioso don de Dios, destinado a elevar los pensamientos a temas más nobles, a inspirar y elevar el alma.
+</p>
+<p>Así como los israelitas cuando andaban por el desierto alegraron su camino con la música del canto sagrado, Dios invita a sus hijos de hoy a alegrar por el mismo medio su vida de peregrinaje. Pocos medios hay más eficaces para grabar sus palabras en la memoria, que el de repetirlas en el canto. Y un canto tal tiene un poder maravilloso. Tiene poder para subyugar naturalezas rudas e incultas; para avivar el pensamiento y despertar simpatía; para promover la armonía de acción y desvanecer la melancolía y los presentimientos que destruyen el valor y debilitan el esfuerzo.
+</p>
+<p>Es uno de los medios más eficaces para grabar en el corazón la verdad espiritual. Cuán a menudo recuerda la memoria al alma oprimida y pronta a desesperar, alguna palabra de Dios, tema olvidado de algún canto de infancia, y las tentaciones pierden su poder, la vida adquiere nuevo significado y nuevo propósito, y se imparte valor y alegría a otras almas.
+</p>
+<p>Nunca se debería perder de vista el valor del canto como medio educativo. Cántense en el hogar cantos dulces y puros, y habrá menos palabras de censura y más de alegría, esperanza y gozo. Cántese en la escuela, y los alumnos serán atraídos más a Dios, a sus maestros, y los unos a los otros.
+</p>
+<p>Como parte del servicio religioso, el canto no es menos importante que la oración, en realidad, más de un canto es una oración.-- <i> Ed, 167-168</i>.
+</p>
+<br>
+<h4>Un arma contra el desánimo</h4>
+<p>Si hubiera mucho más alabanza al Señor y mucho menos tristes relatos de desánimos, se ganarían muchas más victorias. Carta 53, 1896. --Ev, 364.</p>
+<p>Tributemos alabanza y acción de gracias por medio del canto. Cuando nos veamos tentados, en vez de dar expresión a nuestros sentimientos, entonemos con fe un himno de acción de gracias a Dios.
+</p>
+<p>El canto es un arma que siempre podemos esgrimir contra el desaliento. Abriendo así nuestro corazón a los rayos de luz de la presencia del Salvador, encontraremos salud y recibiremos su bendición. -<i>MC, 196 (1905).</i>
+</p>
+<br>
+<h4>Grabar la verdad espiritual</h4>
+<p>El canto es uno de los medios más eficaces para grabar la verdad espiritual en el corazón. A menudo por las palabras de un canto sagrado han nacido sentimientos de contrición y de fe. --<i> RH, Junio 6, 1912.</i></p>
+<br>
+<h4>Para conservar la experiencia cristiana</h4>
+<p>Por la noche y por la mañana uníos con vuestros hijos en el culto a Dios, leyendo su Palabra y cantando sus alabanzas. Enseñadles a repetir la ley de Dios. Respecto de los mandamientos los israelitas recibieron esta instrucción: "Y las repetirás a tus hijos, y hablarás de ellas estando en tu casa, y andando por el camino, y al acostarte, y cuando te levantes." De acuerdo con estas palabras, Moisés instruyó a los israelitas a ponerles música a las palabras de la ley. Mientras los niños mayores tocaban instrumentos musicales, los menores marchaban y cantaban en concierto el cántico de los mandamientos de Dios. En los años subsiguientes retenían en su mente las palabras de la ley que aprendieran durante la niñez.</p>
+<p>Si era esencial para Moisés encarnar los mandamientos en el cántico sagrado, de manera que cuando marcharan por el desierto los niños pudieran aprender la ley versículo por versículo, cuan esencial es en este tiempo enseñar a nuestros hijos la Palabra de Dios. Acudamos en ayuda del Señor, instruyendo a nuestros hijos a guardar los mandamientos al pie de la letra. Hagamos todo lo que esté de nuestra parte para hacer música en nuestro hogar.--<i>Ev, 364-365.</i></p>
+<br>
+<h4>Para hacer el trabajo agradable</h4>
+<p>Alegrad vuestro trabajo con cantos de alabanza. --<i>CN, 136.</i></p>
+<br>
+<h4>Aleja al enemigo</h4>
+<p>Vi que debemos levantarnos cada día y dominar los poderes de las tinieblas. Nuestro Dios es poderoso. Vi que el canto de gloria a Dios a menudo alejaba al enemigo, y el alabar a Dios lo hacía retroceder y nos daba la victoria. --<i>Carta 5, 1850.</i></p>
+<br>
+<h4>El canto ayudó a Jesús a resistir al enemigo</h4>
+<p>Cuando Cristo era niño, como estos niños que están aquí, fue tentado a pecar, pero no se rindió a la tentación. Cuando llegó a tener más edad, fue tentado, pero los cantos que su madre le había enseñado a entonar acudían a su mente, y El elevaba su voz en alabanza. Y antes de que sus compañeros lo advirtieran estaban cantando juntamente con él. Dios quiere que usemos toda facilidad que el cielo nos ha proporcionado, para resistir al enemigo. --<i>Ev, 363.</i></p>
+<br>
+<h4>Trayendo la alegría del cielo</h4>
+<p>El alba le encontraba a menudo en algún retiro, sumido en la meditación, escudriñando las Escrituras, o en oración. Con su canto daba la bienvenida a la luz del día. Con himnos de acción de gracias amenizaba las horas de labor, y llevaba alegría del cielo a los rendidos por el trabajo y a los descorazonados.--<i>MC, 34.</i>
+</p>
+<br>
+<h4>El cantó cantos de alabanza</h4>
+<p>A menudo expresaba su alegría cantando salmos e himnos celestiales. A menudo los moradores de Nazaret oían su voz que se elevaba en alabanza y agradecimiento a Dios. Mantenía comunión con el cielo mediante el canto; y cuando sus compañeros se quejaban por el cansancio, eran alegrados por la dulce melodía que brotaba de sus labios. Sus alabanzas parecían ahuyentar a los malos ángeles, y como incienso, llenaban el lugar de fragancia. La mente de los que le oían se alejaba del destierro que aquí sufrían para elevarse a la patria celestial. --<i>DTG, 54.</i></p>
+</div>
+
+<div id="uso">
+<h3>El uso eficaz de la música en la experiencia de Israel</h3>
+<h4>Los cantos fijaban las lecciones en la mente</h4>
+<p>Mientras el pueblo vagaba por el desierto, el canto era un medio de grabar en sus mentes muchas lecciones preciosas. Cuando fueron librados del ejército de Faraón, toda la hueste de Israel se unió en un canto de triunfo. Por el desierto y el mar resonaron a lo lejos las estrofas de júbilo y en las montañas repercutieron los acentos de alabanza: "Cantad a Jehová, porque en extremo se ha engrandecido" [Exo.15:21]. Con frecuencia se repetía durante el viaje este canto que animaba los corazones y encendía la fe de los peregrinos. Por indicación divina se expresaban también los mandamientos dados desde el Sinaí, con las promesas del favor de Dios y el relato de los milagros que hizo para librarlos, en cantos acompañados de música instrumental, a cuyo compás marchaba el pueblo mientras unía sus voces de alabanza.
+</p>
+<p>De ese modo se apartaban sus pensamientos de las pruebas y dificultades del camino, se calmaba el espíritu inquieto y turbulento, se inculcaban en la memoria los principios de la verdad, y la fe se fortalecía. La acción en concierto servía para enseñar el orden y la unidad, y el pueblo se ponía en más íntima comunión con Dios y con sus semejantes. --<i>Ed, 39.</i></p>
+<h4>En la escuela de los profetas - Parte del programa de estudio</h4>
+<p>Tanto en la escuela como en el hogar, mucha de la enseñanza era oral, pero los jóvenes aprendían también a leer los escritos hebreos, y se ofrecían a su estudio los pergaminos de las Escrituras de Antiguo Testamento. En esas escuelas, los principales temas de estudio eran la ley de Dios, con las instrucciones dadas a Moisés, la historia, la música sagrada y la poesía. -- Ed, 47.
+
+</p>
+</div>
 
           </div>
-          <?php endif; ?>
       </div>
     <div id="menuOp" class="large-3 medium-3 small-12 left">
       <h3>Articulos Relacionados:</h3>
@@ -38,7 +135,7 @@ get_header();
               <p>Titulo de la entrada</p>
             </a>
           </div>
-          <img class="imagenSeleccion" src="img/test.png" alt="">
+          <img class="imagenSeleccion" src="<?php bloginfo('template_url');?>/img/test.png" alt="">
         </div>
       </div>
       <div class="itemMain large-12 medium-12 left columns">
@@ -48,7 +145,7 @@ get_header();
               <p>Sefksde</p>
             </a>
           </div>
-          <img class="imagenSeleccion" src="img/test.png" alt="">
+          <img class="imagenSeleccion" src="<?php bloginfo('template_url');?>/img/test.png" alt="">
         </div>
       </div>
       <div class="itemMain large-12 medium-12 left columns">
@@ -58,7 +155,7 @@ get_header();
               <p>Titulo de la entrada</p>
             </a>
           </div>
-          <img class="imagenSeleccion" src="img/test.png" alt="">
+          <img class="imagenSeleccion" src="<?php bloginfo('template_url');?>/img/test.png" alt="">
         </div>
       </div>
       <div class="itemMain large-12 medium-12 left columns">
@@ -68,7 +165,7 @@ get_header();
               <p>Titulo de la entrada</p>
             </a>
           </div>
-          <img class="imagenSeleccion" src="img/test.png" alt="">
+          <img class="imagenSeleccion" src="<?php bloginfo('template_url');?>/img/test.png" alt="">
         </div>
       </div>
     </div>
